3f2ba70d5ab88d239b352efbef10dd9a8eaee425
Miquel Adell Borràs <miquel@miqueladell.com>
2017-06-09 09:54:25 +0200

    fix search button on mobile

--- src/js/entesa.js
+++ src/js/entesa.js
@@ -1,40 +1,40 @@
 (function($){
-    //cache needed for overagressive garbage collectors.
-    var cache = [];
-    //images can either be an array of paths to images or a  single image.
-    $.loadImages = function(images, callback){
+	//cache needed for overagressive garbage collectors.
+	var cache = [];
+	//images can either be an array of paths to images or a  single image.
+	$.loadImages = function(images, callback){
 
-        // if our first argument is an string, we convert it to an array
-        if (typeof images == "string") {
-            images = [images];
-        }
+		// if our first argument is an string, we convert it to an array
+		if (typeof images == "string") {
+			images = [images];
+		}
 
-        var imagesLength = images.length;
-        var loadedCounter = 0;
+		var imagesLength = images.length;
+		var loadedCounter = 0;
 
-        for (var i = 0; i < imagesLength; i++) {
+		for (var i = 0; i < imagesLength; i++) {
 			var cacheImage = document.createElement('img');
 			//set the onload method before the src is called otherwise will fail to be called in IE
-            cacheImage.onload = function(){
-                loadedCounter++;
-                if (loadedCounter == imagesLength) {
-                    if ($.isFunction(callback)) {
-                        callback();
-                    }
-                }
-            }
-            cacheImage.src = images[i];
-            cache.push(cacheImage);
-        }
-    }
+			cacheImage.onload = function(){
+				loadedCounter++;
+				if (loadedCounter == imagesLength) {
+					if ($.isFunction(callback)) {
+						callback();
+					}
+				}
+			}
+			cacheImage.src = images[i];
+			cache.push(cacheImage);
+		}
+	}
 })(jQuery)
 
 
 jQuery( document ).ready(function( $ ) {
 	$.fn.preload = function() {
-	    this.each(function(){
-	        $('<img/>')[0].src = this;
-	    });
+		this.each(function(){
+			$('<img/>')[0].src = this;
+		});
 	};
 
 
@@ -102,8 +102,8 @@ jQuery( document ).ready(function( $ ) {
 	});
 
 	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-	    event.preventDefault();
-	    $(this).ekkoLightbox();
+		event.preventDefault();
+		$(this).ekkoLightbox();
 	});
 
 	window.sr = ScrollReveal({duration: 500,delay: 300});
